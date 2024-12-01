c54e960246b19eea959b4154264fd50c53d1fe80
Lenny <30445004+blaklinten@users.noreply.github.com>
2024-12-01 21:00:09 +0100

    improve: day01
    - get_input
    - solve2

--- day01/solution.py
+++ day01/solution.py
@@ -1,11 +1,11 @@
 import os
+from collections import Counter
 
 part = os.environ.get("part")
 
 def get_input():
     with open('./input.txt', 'r') as file:
-        lines = file.readlines()
-        return ([int(line.split()[0]) for line in lines], [int(line.split()[1]) for line in lines])
+        return list(zip(*[[int(col) for col in line.split()] for line in file]))
 
 def solve1(input):
     individual_distances = [abs(left - right) for left, right in zip(sorted(input[0]), sorted(input[1]))]
@@ -13,37 +13,8 @@ def solve1(input):
     print(total_distance)
 
 def solve2(input):
-    offset = 0
-    score = 0
-    multiplier = 1
-    right = sorted(input[1])
-    left = sorted(input[0])
-
-    for index in range(len(left)):
-        if index + offset > len(right):
-            print(score)
-            return
-        if left[index] < right[index + offset]:
-            offset -= 1
-            continue
-        while left[index] > right[index + offset]:
-            offset += 1
-            if index + offset >= len(right):
-                print(score)
-                return
-            continue
-        if left[index] == right[index + offset] and index + 1 < len(left) and left[index + 1] == right[index + offset]:
-            multiplier += 1
-            offset -= 1
-            continue
-        while left[index] == right[index + offset]:
-            score += multiplier * left[index]
-            offset += 1
-            if offset > len(right):
-                print(score)
-                return
-        multiplier = 1 
-        offset -= 1
+    counter = Counter(input[1])
+    score = sum([value * counter[value] for value in input[0]])
     print(score)
 
 if part == "part1":
