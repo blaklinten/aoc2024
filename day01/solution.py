b40ad9cab12dd07497682265581dc81abe1cc6b5
Lenny <30445004+blaklinten@users.noreply.github.com>
2024-12-01 21:26:05 +0100

    cred where cred is due
    all hail Rembane, our functional saviour!

--- day01/solution.py
+++ day01/solution.py
@@ -5,6 +5,7 @@ part = os.environ.get("part")
 
 def get_input():
     with open('./input.txt', 'r') as file:
+        # Thanks to Rembane for this elegant solution to skip double iterators and teaching me about splat operator :D
         return list(zip(*[[int(col) for col in line.split()] for line in file]))
 
 def solve1(input):
@@ -13,6 +14,7 @@ def solve1(input):
     print(total_distance)
 
 def solve2(input):
+    # Also thanks Rembane for showing me the Counter class and guide me on how to use it
     counter = Counter(input[1])
     score = sum([value * counter[value] for value in input[0]])
     print(score)
